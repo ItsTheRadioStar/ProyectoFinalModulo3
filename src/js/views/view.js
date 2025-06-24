1857f387ca930b9a5393b1bd45a9010a85036686
ItsTheRadioStar <mgr921101@gmail.com>
2025-06-23 23:43:56 -0600

    Cambios a view.js

--- src/js/views/view.js
+++ src/js/views/view.js
@@ -1,36 +1,35 @@
 import icons from '../../img/icons.svg'
 
 export default class View {
-    _data;
-    _parentElement;
+  _data;
+  _parentElement;
 
-    render(data) {
-        this._data = data
-        const markup = this._generateMarkup();
-        if (!data || (Array.isArray(data) && data.length === 0))
-            return this.renderError();
-        this._clear();
-        this._parentElement.insertAdjacentHTML('afterbegin', this._generateMarkup())
-    }
+  render(data) {
+    if (!data || (Array.isArray(data) && data.length === 0))
+      return this.renderError();
+    this._data = data
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', this._generateMarkup())
+  }
 
-    _clear() {
-        this._parentElement.innerHTML = '';
-    }
+  _clear() {
+    this._parentElement.innerHTML = '';
+  }
 
-    renderSpinner() {
-        const markup = `
+  renderSpinner() {
+    const markup = `
                 <div class="spinner">
                   <svg>
                     <use href="${icons}#icon-loader"></use>
                   </svg>
                 </div>
               `;
-        this._clear();
-        this._parentElement.insertAdjacentHTML('afterbegin', markup);
-    }
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
 
-    renderError(message = this._errorMessage) {
-        const markup = `<div class="error">
+  renderError(message = this._errorMessage) {
+    const markup = `<div class="error">
                 <div>
                   <svg>
                     <use href="${icons}#icon-alert-triangle"></use>
@@ -38,12 +37,12 @@ export default class View {
                 </div>
                 <p>${message}</p>
               </div>`;
-        this._clear();
-        this._parentElement.insertAdjacentHTML('afterbegin', markup);
-    }
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
 
-    renderMessage(message = this._Message) {
-        const markup = `<div class="message">
+  renderMessage(message = this._Message) {
+    const markup = `<div class="message">
                 <div>
                   <svg>
                     <use href="${icons}#icon-smile"></use>
@@ -51,7 +50,7 @@ export default class View {
                 </div>
                 <p>${message}</p>
               </div>`;
-        this._clear();
-        this._parentElement.insertAdjacentHTML('afterbegin', markup);
-    }
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
 }
